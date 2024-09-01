4622c52d2abfc62e5f182662e1c151f54f8e68aa
BashaarIltaadi <kantesh.125@gmail.com>
2024-09-02 01:24:28 +0500

    "Updated account models, added chat functionality, and modified templates and static files, initial commit"

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,45 @@
+/* Variables*/
+let chatName = ''
+let chatSocket = null
+let chatWindowUrl = window.location.href
+let chatRoomUuid = Math.random().toString(36).slice(2, 12)
 
+
+/* Elements */
+
+const chatElement = document.querySelector('#chat')
+const chatOpenElement = document.querySelector("#chat_open")
+const chatJoinElement = document.querySelector("#chat_join")
+const chatIconElement = document.querySelector("#chat_icon")
+const chatWelcomeElement = document.querySelector("#chat_welcome")
+const chatRoomElement = document.querySelector("#chat_room")
+const chatLogElement = document.querySelector("#chat_log")
+const chatInputElement = document.querySelector("#chat_message_input")
+const chatSubmitElement = document.querySelector("#chat_message_submit")
+
+
+/* Functions */
+
+function joinChatRoom() {
+    console.log('joinchatroom()')
+
+}
+
+/* Event Listeners */
+chatOpenElement.onclick = function(e){
+    e.preventDefault()
+
+    chatIconElement.classList.add('hidden')
+    chatWelcomeElement.classList.remove('hidden')
+
+    return false
+}
+
+chatJoinElement.onclick = function(e){
+    e.preventDefault()
+
+    chatWelcomeElement.classList.add('hidden')
+    chatRoomElement.classList.remove('hidden')
+    joinChatRoom()
+    return false
+}
